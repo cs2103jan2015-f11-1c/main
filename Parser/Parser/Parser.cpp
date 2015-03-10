dcb7c4e7015c631e0a8122404140b4db761af165
JieYang <a0111642@nus.edu.sg>
2015-03-10 15:40:06 +0800

    Parser::Test

--- Parser/Parser/Parser.cpp
+++ Parser/Parser/Parser.cpp
@@ -4,18 +4,19 @@
 #include <vector>
 using namespace std;
 
+
 Parser::Parser()
 {}
 
 
 void Parser::sortCommand()
 {
-
+ return;
 }
 
-void Parser::sortDetails()
+int Parser::sortDetails()
 {
-
+	return 1;
 }
 
 
