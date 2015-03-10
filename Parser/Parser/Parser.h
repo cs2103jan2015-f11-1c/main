d49b6838226b85f76325340b37785a10c611189c
Liu Gantian <gantian.here@gmail.com>
2015-03-10 18:43:57 +0800

    Logic:: Edited parser file V2

--- Parser/Parser/Parser.h
+++ Parser/Parser/Parser.h
@@ -6,21 +6,10 @@
 //using namespace std;
 
 class Parser{
-<<<<<<< HEAD
-	public: 
-//Parser();
-//paraList para;
-=======
- 
-
-private:
-
-
 
 public:
 /*Parser();*/
-paraList para;
->>>>>>> c21bc99438ca11c4e21fb29a3898ad614d73e684
+//paraList para;
 /* void sortCommand(string &userInput);
 void sortDetails(string &userInput);*/
 string parseCommand(string userInput);
