8961a86d97aeacbd2f544b58e189c4bc1c0626db
Liu Gantian <gantian.here@gmail.com>
2015-03-10 14:27:43 +0800

    Logic:: Logic.h file include directories for Parser Storage UI

---
+++ Logic/Logic/Logic.cpp
@@ -0,0 +1,15 @@
+#include "Logic.h"
+#include "..\..\Storage\Storage\Storage.h"
+#include "..\..\Storage\Storage\Storage.cpp"
+
+
+
+int main(){
+
+	Storage Jieyang;
+	cout<<Jieyang.print();
+
+	system("pause");
+	return 0;
+
+}
---
+++ Logic/Logic/Logic.h
@@ -0,0 +1,21 @@
+#ifndef LOGIC_H
+#define LOGIC_H
+
+#include "..\..\Storage\Storage\Storage.h"
+#include "..\..\Storage\Storage\Storage.cpp"
+#include "..\..\UI\UI\UI.h"
+#include "..\..\UI\UI\UI.cpp"
+#include "..\..\Parser\Parser\Parser.h"
+#include "..\..\Parser\Parser\Parser.cpp"
+
+
+
+
+
+
+
+
+
+
+
+#endif
