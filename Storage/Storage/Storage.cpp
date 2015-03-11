95aa61fea1c5e30e781a5250841e37da41565361
Andy Koh <kohchongkeng@gmail.com>
2015-03-11 19:08:37 +0800

    Storage/Task::Added getter functions & addTask details

--- Storage/Storage/Storage.cpp
+++ Storage/Storage/Storage.cpp
@@ -1,16 +1,40 @@
 #include "Storage.h"
-
-//string taskName, taskStartDate, taskStartTime, taskEndDate, taskEndTime, taskDeadlineDate, taskDeadlineTime, taskPriority;
+#include "..\..\Task\Task\Task.h"
 
 //vector<Task*> getTaskList(){
 //	return taskList;
 //}
 
 void Storage::addTask(Task *individual_task){
-//Task *individualTask = new Task(taskName,taskStartDate,taskStartTime,taskEndDate,taskEndTime,taskDeadlineDate,taskDeadlineTime,taskPriority);
-	//individual_task->changeTaskName(individual_task->_taskName);
+
+	string taskName = individual_task->getTaskName(); 
+	string taskStartDate = individual_task->getTaskStartDate();
+	string taskStartTime = individual_task->getTaskStartTime();
+	string taskEndDate = individual_task->getTaskEndDate();
+	string taskEndTime = individual_task->getTaskEndTime();
+	string taskDeadlineDate = individual_task->getTaskDeadlineDate();
+	string taskDeadlineTime = individual_task->getTaskDeadlineTime();
+	string taskPriority = individual_task->getTaskPriority();
+
 	taskList.push_back(individual_task); 
-	vector<Task*>::iterator iter = taskList.end();
-	//taskList[0]->changeTaskName(individual_task->_taskName);
-	(**iter).changeTaskName(individual_task->_taskName);
+
+	//vector<Task*>::iterator iter = taskList.end();
+	//(**iter).changeTaskName(taskName);
+	//string b=(**iter).getTaskName();
+	//cout<<b;
+	
+	taskList[0]->changeTaskName(taskName);
+	taskList[0]->changeTaskStartDate(taskStartDate);
+	taskList[0]->changeTaskStartTime(taskStartTime);
+	taskList[0]->changeTaskEndDate(taskEndDate);
+	taskList[0]->changeTaskEndTime(taskEndTime);
+	taskList[0]->changeTaskDeadlineDate(taskDeadlineDate);
+	taskList[0]->changeTaskDeadlineTime(taskDeadlineTime);
+	taskList[0]->changeTaskPriority(taskPriority);
+
+	string taskNameOutput=taskList[0]->getTaskName();
+	cout<<taskNameOutput<<endl;
+	string taskPriorityOutput=taskList[0]->getTaskPriority();
+	cout<<taskPriorityOutput<<endl;
+
 }
