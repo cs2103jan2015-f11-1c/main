23db4ae3ff4b43466d421949f59be24d0948595e
Asyraf Akbar <mohamed.asyraf@gmail.com>
2015-03-11 18:21:32 +0800

    UI:: Added more error messages

--- UI/UI/UI.cpp
+++ UI/UI/UI.cpp
@@ -18,9 +18,33 @@ void UI::displayPromptInputMessage() {
 	return;
 }
 
+void UI::displayInvalidCommandMessage() {
+
+	cout << _invalidCommandMessage;
+	return;
+}
+
+void UI::displayDeleteErrorMessage() {
+
+	cout << _deleteErrorMessage;
+	return;
+}
+
+
+
+void UI::displayTask(vector<string> taskString) {
+
+	vector<string>::iterator iter;
+	for(iter = taskString.begin(); iter != taskString.end(); ++iter) {
+		cout << *iter << " ";
+	}
+
+	return;
+}
+
 string UI::acceptUserInput() {
 
 	string userInput;
-	cin >> userInput;
+	getline(cin, userInput);
 	return userInput;
 }
--- UI/UI/UI.h
+++ UI/UI/UI.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -11,13 +12,22 @@ private:
 
 	string _welcomeMessage = "Welcome to TaskSotong!"; 
 	string _exitMessage = "Goodbye!";
+
 	string _promptInputMessage = "What would you like to do?";
+
+	string _invalidCommandMessage = "Error, command not recognized. Please enter a valid command! (add, update, delete, sort, exit)";
+	string _deleteErrorMessage = "Error. Please enter a valid index!"
 
 public:
 
 	void displayWelcomeMessage();
 	void displayExitMessage();
 	void displayPromptInputMessage();
+
+	void displayInvalidCommandMessage();
+	void displayDeleteErrorMessage();
+
+	void displayTask(vector<string> taskString);
 
 	string acceptUserInput();
 };
