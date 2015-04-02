c713e67fb8656b8355699bd9ba16dd7c54051d4e
Liu Gantian <gantian.here@gmail.com>
2015-04-03 01:45:04 +0800

    V0.2 ::Logic:: refactored

--- Logic/Logic.cpp
+++ Logic/Logic.cpp
@@ -7,17 +7,17 @@
 
 using namespace std;
 
-string Logic::getUserInput(){
+string Logic::getUserInput() {
 	return UserInterface.acceptUserInput();
 }
 
-void Logic::Welcome(){
+void Logic::Welcome() {
 	UserInterface.displayWelcomeMessage();
 	return;
 }
 
 //this function only gets the directory of the .exe file, not where the .txt file is. 
-string Logic::getExePath(){
+string Logic::getExePath() {
 
 	char buffer[MAX_PATH];
 	GetModuleFileName(NULL, buffer, MAX_PATH);
@@ -58,7 +58,7 @@ return false;
 }
 */
 
-void Logic::createNewDirectory(string userFileDirectory){
+void Logic::createNewDirectory(string userFileDirectory) {
 
 	int index = userFileDirectory.find_last_of('\\');
 	int index2 = userFileDirectory.find_first_of('\\');
@@ -66,15 +66,14 @@ void Logic::createNewDirectory(string userFileDirectory){
 
 	stack<string> directoryParts;
 	int firstIndex = inputDirectory.find_last_of('\\');
-	while (firstIndex != -1){
+	while (firstIndex != -1) {
 
 		firstIndex = inputDirectory.find_last_of('\\');
 		string temp = inputDirectory.substr(firstIndex, inputDirectory.size() - 1);
 
-		if (temp == "\\"){
+		if (temp == "\\") {
 
-		}
-		else{
+		} else {
 			directoryParts.push(temp);
 		}
 
@@ -88,7 +87,7 @@ void Logic::createNewDirectory(string userFileDirectory){
 	string directory = directoryParts.top();
 	directoryParts.pop();
 
-	while (!directoryParts.empty()){
+	while (!directoryParts.empty()) {
 		//for debugging purpose
 		//cout << directoryParts.top() << endl;
 		directory = directory + directoryParts.top();
@@ -101,7 +100,7 @@ void Logic::createNewDirectory(string userFileDirectory){
 	return;
 }
 
-void Logic::changeFileDirectory(string userFileDirectory){
+void Logic::changeFileDirectory(string userFileDirectory) {
 
 	createNewDirectory(userFileDirectory);
 	setFileName(userFileDirectory);
@@ -111,128 +110,118 @@ void Logic::changeFileDirectory(string userFileDirectory){
 
 }
 
-void Logic::processChangeDirectoryRequest(string userFileDirectory){
+void Logic::processChangeDirectoryRequest(string userFileDirectory) {
 
-		changeFileDirectory(userFileDirectory);
+	changeFileDirectory(userFileDirectory);
 	return;
 
 }
 
-void Logic::setFileName(string updatedFileName){
+void Logic::setFileName(string updatedFileName) {
 	_filename = updatedFileName;
 	return;
 }
 
 
-string Logic::getFileName(){
+string Logic::getFileName() {
 	return _filename;
 }
 
-void Logic::CommandPrompt(){
+void Logic::CommandPrompt() {
 	UserInterface.displayPromptInputMessage();
 	return;
 }
 
-paraList* Logic::getParaList(string userInput){
+paraList* Logic::getParaList(string userInput) {
 
 	return ParserComponent.parseCommand(userInput);
 }
 
 
-string Logic::getCommand(paraList parameterList){
+string Logic::getCommand(paraList parameterList) {
 	string command = parameterList.getCommand();
 	transform(command.begin(), command.end(), command.begin(), tolower);
 	return command;
 
 }
 
-Task Logic::getTask(paraList parameterList){
+Task Logic::getTask(paraList parameterList) {
 	return parameterList.getTask();
 }
 
-void Logic::copyTestFilefromStorage(){
+void Logic::copyTestFilefromStorage() {
 	textFileCopy_fromStorage = getTextFileCopy();
 	return;
 }
 
-vector<string> Logic::getTextFileCopy(){
+vector<string> Logic::getTextFileCopy() {
 	return DataBase.returnTextFileCopy();
 }
 
-void Logic::callInitialise(string outputFile){
+void Logic::callInitialise(string outputFile) {
 	DataBase.initialiseTextFile(outputFile);
 }
 
-bool Logic::notExistingTask(Task* task){
+bool Logic::notExistingTask(Task* task) {
 	string taskDetail;
 	taskDetail = task->getTaskDetails();
-	for (unsigned int i = 0; i < textFileCopy_fromStorage.size(); i++){
-		if (taskDetail == textFileCopy_fromStorage[i]){
+	for (unsigned int i = 0; i < textFileCopy_fromStorage.size(); i++) {
+		if (taskDetail == textFileCopy_fromStorage[i]) {
 			return false;
 		}
 	}
 	return true;
 }
 
-void Logic::executeCommand(paraList Input){
+void Logic::executeCommand(paraList Input) {
 
 	string command = Input.getCommand();
 
 	Task oneTask = Input.getTask();
 
-	if (command == "invalid"){
+	if (command == "invalid") {
 		UserInterface.displayInvalidCommandMessage();
-	}
-	else if (command == "add"){
-		if (notExistingTask(&oneTask)){
+	} else if (command == "add") {
+		if (notExistingTask(&oneTask)) {
 			DataBase.addTask(&oneTask);
 			DataBase.updateTextFile(_filename);
 			UserInterface.displaySuccessfulAddMessage();
-		}
-		else{
+		} else {
 			cout << "Existing Task! Please enter a new task! :(" << endl;
 		}
-	}
-	else if (command == "display"){
+	} else if (command == "display") {
 		DataBase.displayAllTasks();
 		DataBase.updateTextFile(_filename);
-	}
-	else if (command == "update"){
+	} else if (command == "update") {
 		int updateInteger = Input.getUpdateInteger();
 		string keyword1 = Input.getKeyword();
 		string detail = Input.getInput();
 		DataBase.updateTask(updateInteger, keyword1, detail);
 		DataBase.updateTextFile(_filename);
 		UserInterface.displaySuccessfulUpdateMessage();
-	}
-	else if (command == "delete"){
+	} else if (command == "delete") {
 		int deleteInteger = Input.getDeleteInteger();
 		DataBase.deleteTask(deleteInteger);
 		DataBase.updateTextFile(_filename);
 		UserInterface.displaySuccessfulDeleteMessage();
-	}
-	else if (command == "save"){
+	} else if (command == "save") {
 
 		string userDirectory = Input.getuserdir();
 		processChangeDirectoryRequest(userDirectory);
 		cout << "Saving derectory changed! :D" << endl;
-	}
-	else if (command == "mark"){
+	} else if (command == "mark") {
 		int markIndex = Input.getmarkindex();
 		DataBase.markTask(markIndex, "mark");
 		DataBase.updateTextFile(_filename);
-	}
-	else if (command == "unmark"){
+	} else if (command == "unmark") {
 
 		int markIndex = Input.getmarkindex();
 
 		DataBase.markTask(markIndex, "unmark");
 		DataBase.updateTextFile(_filename);
-	}
-	else if (command == "clear"){
+	} else if (command == "clear") {
 		DataBase.clearAllTasks();
-	}
-	else if (command == "undo"){
+	} else if (command == "undo") {
 		DataBase.undoAction();
 		DataBase.updateTextFile(_filename);
 	}
