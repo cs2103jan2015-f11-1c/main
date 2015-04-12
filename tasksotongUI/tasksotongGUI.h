406ad130bdf90434d4e15d8ba2038c40e59bf60c
Liu Gantian <gantian.here@gmail.com>
2015-04-13 03:11:00 +0800

    V0.5::GUI:: search doesnt really work nicely.....

--- tasksotongUI/tasksotongGUI.h
+++ tasksotongUI/tasksotongGUI.h
@@ -513,7 +513,6 @@ namespace tasksotongUI {
 
 			//DisplayTaskBox->Text = textFileCopy_fromLogic.size().ToString();
 
-			string outputDisplay;
 			Display_richTextBox->Clear();
 
 			for (int i = 0; i < taskListCopy_fromLogic.size(); i++) {
@@ -656,12 +655,10 @@ namespace tasksotongUI {
 	private: System::Void tasksotongGUI_Load(System::Object^  sender, System::EventArgs^  e) {
 
 		TSLogic->initialiseSetUp();
-		TSLogic->setTaskList();
-		
+		TSLogic->setTaskList();	
 		
 		vector<Task> taskListCopy_fromLogic = TSLogic->getTaskList();
-		//string test = "Size of taskList: " + to_string(taskListCopy_fromLogic.size());
-		//String^ display = gcnew String(test.c_str());
+
 		
 		if (taskListCopy_fromLogic.size() == 0) {
 			
@@ -808,64 +805,3 @@ namespace tasksotongUI {
 
 
 
-/*#pragma once
-
-namespace tasksotongUI {
-
-using namespace System;
-using namespace System::ComponentModel;
-using namespace System::Collections;
-using namespace System::Windows::Forms;
-using namespace System::Data;
-using namespace System::Drawing;
-
-/// <summary>
-/// Summary for tasksotongGUI
-/// </summary>
-public ref class tasksotongGUI : public System::Windows::Forms::Form
-{
-public:
-tasksotongGUI(void)
-{
-InitializeComponent();
-//
-//TODO: Add the constructor code here
-//
-}
-
-protected:
-/// <summary>
-/// Clean up any resources being used.
-/// </summary>
-~tasksotongGUI()
-{
-if (components)
-{
-delete components;
-}
-}
-
-private:
-/// <summary>
-/// Required designer variable.
-/// </summary>
-System::ComponentModel::Container ^components;
-
-#pragma region Windows Form Designer generated code
-/// <summary>
-/// Required method for Designer support - do not modify
-/// the contents of this method with the code editor.
-/// </summary>
-void InitializeComponent(void)
-{
-this->components = gcnew System::ComponentModel::Container();
-this->Size = System::Drawing::Size(300,300);
-this->Text = L"tasksotongGUI";
-this->Padding = System::Windows::Forms::Padding(0);
-this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-}
-#pragma endregion
-};
-}
-
-*/
