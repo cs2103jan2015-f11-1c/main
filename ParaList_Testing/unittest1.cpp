91ed4fdd5bc903eb4d4bbbfdf7516a251b05e0d9
Liu Gantian <gantian.here@gmail.com>
2015-04-13 22:15:44 +0800

    V0.5:: Load all unit testing

--- ParaList_Testing/unittest1.cpp
+++ ParaList_Testing/unittest1.cpp
@@ -3,222 +3,205 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace ParaList_Testing
-{		
-	TEST_CLASS(ParaList_Testing)
-	{
-	public:
-		
-		TEST_METHOD(getTask_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
-		
-			Task actual= test.getTask();
-			Task* testTask = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			Task expected = *testTask;
+namespace ParaList_Testing {
+	TEST_CLASS(ParaList_Testing) {
+public:
 
-			string actual1 = actual.getTaskDetails();
-			string expected1 = expected.getTaskDetails();
-			
-			Assert::AreEqual(actual1, expected1);
-		}
+	TEST_METHOD(getTask_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
 
-		TEST_METHOD(getCommand_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
+		Task actual = test.getTask();
+		Task* testTask = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		Task expected = *testTask;
+
+		string actual1 = actual.getTaskDetails();
+		string expected1 = expected.getTaskDetails();
+
+		Assert::AreEqual(actual1, expected1);
+	}
+
+	TEST_METHOD(getCommand_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
 
 
-			string actual = test.getCommand();
-			string expected = "add";
+		string actual = test.getCommand();
+		string expected = "add";
 
-			Assert::AreEqual(actual, expected);
-		}
+		Assert::AreEqual(actual, expected);
+	}
 
-		TEST_METHOD(getDeleteInteger_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
-
-
-			int actual = test.getDeleteInteger();
-			int expected = 1;
-
-			Assert::AreEqual(actual, expected);
-			
-			//This is a boundary testing for getDeleteInteger. 
-			//when the user key in delete 0, it is an invalid index, just outside the boundary
-			paraList test1;
-			Task* taskTest1 = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test1.setparaList("add", 0, 2, 3, "KEYWORD", "INPUT", *taskTest);
+	TEST_METHOD(getDeleteInteger_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
 
 
-			int actual1 = test.getDeleteInteger();
-			int expected1 = 0;
+		int actual = test.getDeleteInteger();
+		int expected = 1;
 
-			Assert::AreEqual(actual, expected);
+		Assert::AreEqual(actual, expected);
 
-		}
+		//This is a boundary testing for getDeleteInteger. 
+		//when the user key in delete 0, it is an invalid index, just outside the boundary
+		paraList test1;
+		Task* taskTest1 = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test1.setparaList("add", 0, 2, 3, "KEYWORD", "INPUT", *taskTest);
 
-		TEST_METHOD(getDisplayInteger_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
+		int actual1 = test.getDeleteInteger();
+		int expected1 = 0;
 
+		Assert::AreEqual(actual, expected);
 
-			int actual = test.getDisplayInteger();
-			int expected = 2;
+	}
 
-			Assert::AreEqual(actual, expected);
-		}
-
-		TEST_METHOD(getUpdateInteger_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
+	TEST_METHOD(getDisplayInteger_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
 
 
-			int actual = test.getUpdateInteger();
-			int expected = 3;
+		int actual = test.getDisplayInteger();
+		int expected = 2;
 
-			Assert::AreEqual(actual, expected);
-		}
+		Assert::AreEqual(actual, expected);
+	}
 
-		TEST_METHOD(processCommand_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
-
-			test.processCommand("delete");
-			string actual = test.getCommand();
-			string expected = "delete";
-
-			Assert::AreEqual(actual, expected);
-		}
-
-		//void processTask(string inputTask);
-
-		TEST_METHOD(processDeleteNumber_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
-
-			test.processDeleteNumber(2);
-			int actual = test.getDeleteInteger();
-			int expected = 2;
-
-			Assert::AreEqual(actual, expected);
-		}
-
-		TEST_METHOD(processDisplayNumber_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
-
-			test.processDisplayNumber(3);
-			int actual = test.getDisplayInteger();
-			int expected = 3;
-
-			Assert::AreEqual(actual, expected);
-		}
-
-		TEST_METHOD(processUpdateNumber_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
-
-			test.processUpdateNumber(1);
-			int actual = test.getUpdateInteger();
-			int expected = 1;
-
-			Assert::AreEqual(actual, expected);
-		}
-
-		TEST_METHOD(clearAllNumber_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
-
-			test.clearAllNumber();
-
-			int actual = test.getDeleteInteger();
-			int expected = NULL;
-
-			int actual1 = test.getDisplayInteger();
-			int expected1 = NULL;
-
-			int actual2 = test.getUpdateInteger();
-			int expected2 = NULL;
-
-			Assert::AreEqual(actual, expected);
-			Assert::AreEqual(actual1, expected1);
-			Assert::AreEqual(actual2, expected2);
-		}
-
-		TEST_METHOD(getKeyword_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
-
-			
-			string actual = test.getKeyword();
-			string expected = "KEYWORD";
-
-			Assert::AreEqual(actual, expected);
-		}
+	TEST_METHOD(getUpdateInteger_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
 
 
-		TEST_METHOD(getInput_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
+		int actual = test.getUpdateInteger();
+		int expected = 3;
+
+		Assert::AreEqual(actual, expected);
+	}
+
+	TEST_METHOD(processCommand_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
+
+		test.processCommand("delete");
+		string actual = test.getCommand();
+		string expected = "delete";
+
+		Assert::AreEqual(actual, expected);
+	}
+
+	//void processTask(string inputTask);
+
+	TEST_METHOD(processDeleteNumber_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
+
+		test.processDeleteNumber(2);
+		int actual = test.getDeleteInteger();
+		int expected = 2;
+
+		Assert::AreEqual(actual, expected);
+	}
+
+	TEST_METHOD(processDisplayNumber_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
+
+		test.processDisplayNumber(3);
+		int actual = test.getDisplayInteger();
+		int expected = 3;
+
+		Assert::AreEqual(actual, expected);
+	}
+
+	TEST_METHOD(processUpdateNumber_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
+
+		test.processUpdateNumber(1);
+		int actual = test.getUpdateInteger();
+		int expected = 1;
+
+		Assert::AreEqual(actual, expected);
+	}
+
+	TEST_METHOD(clearAllNumber_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
+
+		test.clearAllNumber();
+
+		int actual = test.getDeleteInteger();
+		int expected = NULL;
+
+		int actual1 = test.getDisplayInteger();
+		int expected1 = NULL;
+
+		int actual2 = test.getUpdateInteger();
+		int expected2 = NULL;
+
+		Assert::AreEqual(actual, expected);
+		Assert::AreEqual(actual1, expected1);
+		Assert::AreEqual(actual2, expected2);
+	}
+
+	TEST_METHOD(getKeyword_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
 
 
-			string actual = test.getInput();
-			string expected = "INPUT";
+		string actual = test.getKeyword();
+		string expected = "KEYWORD";
 
-			Assert::AreEqual(actual, expected);
-		}
+		Assert::AreEqual(actual, expected);
+	}
 
-		TEST_METHOD(processKeyWord_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
 
-			test.processKeyWord("KEYWORD!");
-			string actual = test.getKeyword();
-			string expected = "KEYWORD!";
+	TEST_METHOD(getInput_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
 
-			Assert::AreEqual(actual, expected);
-		}
 
-		TEST_METHOD(processInput_test)
-		{
-			paraList test;
-			Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
-			test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
+		string actual = test.getInput();
+		string expected = "INPUT";
 
-			test.processInput("INPUT!");
-			string actual = test.getInput();
-			string expected = "INPUT!";
+		Assert::AreEqual(actual, expected);
+	}
 
-			Assert::AreEqual(actual, expected);
-		}
-		
+	TEST_METHOD(processKeyWord_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
+
+		test.processKeyWord("KEYWORD!");
+		string actual = test.getKeyword();
+		string expected = "KEYWORD!";
+
+		Assert::AreEqual(actual, expected);
+	}
+
+	TEST_METHOD(processInput_test) {
+		paraList test;
+		Task* taskTest = new Task("Name", "StartDate", "StartTime", "EndDate", "EndTime", "DeadlineDate", "DeadlineTime", "Priority", "Incompleted");
+		test.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *taskTest);
+
+		test.processInput("INPUT!");
+		string actual = test.getInput();
+		string expected = "INPUT!";
+
+		Assert::AreEqual(actual, expected);
+	}
+
 
 	};
 }
--- addTaskTest/unittest1.cpp
+++ addTaskTest/unittest1.cpp
@@ -3,130 +3,64 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace Logic_Test
-{
-	TEST_CLASS(Logic_Test)
-	{
-	public:
+namespace Logic_Test {
+	TEST_CLASS(Logic_Test) {
+public:
 
-		TEST_METHOD(executeCommand_add)
-		{
-			Logic testLogic;
+	TEST_METHOD(executeCommand_add) {
+		Logic testLogic;
 
-			Task* test = new Task("Andy's bday", "14/4/2015", "13:00", "14/4/2015", "14:00", "", "", "!h", "");
-			Task* test1 = new Task("Andy's bday1", "14/4/2015", "13:00", "14/4/2015", "14:00", "", "", "!h", "");
-			Task* test1_copy = new Task("Andy's bday1", "14/4/2015", "13:00", "14/4/2015", "14:00", "", "", "!h", "");
+		Task* test = new Task("Andy's bday", "14/4/2015", "13:00", "14/4/2015", "14:00", "", "", "!h", "");
+		Task* test1 = new Task("Andy's bday1", "14/4/2015", "13:00", "14/4/2015", "14:00", "", "", "!h", "");
+		Task* test1_copy = new Task("Andy's bday1", "14/4/2015", "13:00", "14/4/2015", "14:00", "", "", "!h", "");
 
+		paraList testParaList;
+		paraList testParaList1;
+		paraList testParaList1_copy;
 
-			paraList testParaList;
-			paraList testParaList1;
-			paraList testParaList1_copy;
+		testParaList.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *test);
+		testParaList1.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *test1);
+		testParaList1_copy.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *test1_copy);
 
+		string feedBack = testLogic.executeCommand(testParaList);
+		string feedBack1 = testLogic.executeCommand(testParaList1);
+		string feedBack1_copy = testLogic.executeCommand(testParaList1_copy);
 
-			testParaList.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *test);
-			testParaList1.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *test1);
-			testParaList1_copy.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *test1_copy);
+		string actualOutputString = feedBack;
+		string actualOutputString1 = feedBack1;
+		string expectedOutputString = "Task Added Successfully! :>";
 
+		Assert::AreEqual(expectedOutputString, actualOutputString);
+		Assert::AreEqual(expectedOutputString, actualOutputString1);
 
-			string feedBack = testLogic.executeCommand(testParaList);
-			string feedBack1 = testLogic.executeCommand(testParaList1);
-			string feedBack1_copy = testLogic.executeCommand(testParaList1_copy);
+	}
 
-			
-			string actualOutputString = feedBack;
-			string actualOutputString1 = feedBack1;
-			string expectedOutputString = "Task Added Successfully! :>";
+	TEST_METHOD(executeCommand_delete) {
 
-			
-			Assert::AreEqual(expectedOutputString, actualOutputString);
-			Assert::AreEqual(expectedOutputString, actualOutputString1);
-			/*
-			string actualOutputString2 = feedBack1_copy;
-			string expectedOutputString1 = "That Task Has Already Existed~! Please Enter A New Task~";
-			Assert::AreEqual(expectedOutputString1, actualOutputString2);
-			*/
-			
+		Logic testLogic;
 
-		}
-		/*
-		TEST_METHOD(executeCommand_display) {
-			
-			Logic testLogic;
+		Task* test = new Task("Andy's bday", "10/4/2015", "13:00", "10/4/2015", "14:00", "11/4/2015", "00:00", "high", "Incompleted");
+		Task* test1 = new Task("Andy's bday1", "10/4/2015", "13:00", "10/4/2015", "14:00", "11/4/2015", "00:00", "high", "Incompleted");
 
-			Task* test = new Task("Andy's bday", "10/04/2015", "1300", "10/04/2015", "1400", "11/04/2015", "0000", "high", "Incompleted");
-			Task* test1 = new Task("Andy's bday1", "10/04/2015", "1300", "10/04/2015", "1400", "11/04/2015", "0000", "high", "Incompleted");
+		paraList testParaList;
+		paraList testParaList1;
+		paraList testParaList2;
 
-			paraList testParaList;
-			paraList testParaList1;
-			paraList testParaList2;
+		testParaList.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *test);
+		testParaList1.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *test1);
+		testParaList2.setparaList("delete", 1, 1, 1, "KEYWORD", "INPUT", *test1);
 
-			testParaList.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *test);
-			testParaList1.setparaList("add", 1, 2, 3, "KEYWORD", "INPUT", *test1);
-			testParaList2.setparaList("display", 1, 2, 3, "KEYWORD", "INPUT", *test1);
+		testLogic.setTaskList();
+		string feedBack1 = testLogic.executeCommand(testParaList);
+		testLogic.setTaskList();
+		string feedBack2 = testLogic.executeCommand(testParaList1);
+		testLogic.setTaskList();
+		string feedBack = testLogic.executeCommand(testParaList2);
 
-			string feedBack1 = testLogic.executeCommand(testParaList);
-			string feedBack2 = testLogic.executeCommand(testParaList1);
-			string feedBack = testLogic.executeCommand(testParaList2);
-		
+		string expectedOutputString = feedBack;
+		string actualOutputString = "Task Deleted Successfully!";
 
-			string expectedOutputString = feedBack;
-			string actualOutputString = "All Tasks Are Displayed!";
-
-			Assert::AreEqual(expectedOutputString, actualOutputString);
-
-
-			vector<string> actualOutputVector = testLogic.getReturnGUI();
-			vector<string> expectedOutputVector;
-			expectedOutputVector.push_back("Andy's bday    10/04/2015 1300 10/04/2015 1400 11/04/2015 0000 high Incompleted");
-			expectedOutputVector.push_back("Andy's bday1   10/04/2015 1300 10/04/2015 1400 11/04/2015 0000 high Incompleted");
-
-			Assert::AreEqual(expectedOutputVector[0], actualOutputVector[0]);
-			Assert::AreEqual(expectedOutputVector[1], actualOutputVector[1]);
-		}
-		
-		*/
-
-
-
+		Assert::AreEqual(expectedOutputString, actualOutputString);
+	}
 	};
 }
-
-/*
-void Welcome();
-
-void CommandPrompt();
-
-string getExePath();
-
-//bool validDirectory(string);
-
-void createNewDirectory(string);
-
-void changeFileDirectory(string);
-
-void processChangeDirectoryRequest(string);
-
-string getFileName();
-
-paraList* getParaList(string);
-
-//void writeFile(vector<string>, string);
-
-string getUserInput();
-
-string getCommand(paraList);
-
-Task getTask(paraList);
-
-void copyTestFilefromStorage();
-
-vector<string> getTextFileCopy();
-
-void callInitialise(string);
-
-bool notExistingTask(Task*);
-
-void executeCommand(paraList, string);
-
-void displayFeedback(string);
-*/
